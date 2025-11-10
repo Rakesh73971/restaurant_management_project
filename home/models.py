0bba0c10ee50ddee47c00add07f3bbaab413b026
Rakesh73971 <nraki522@gmail.com>
2025-11-10 20:44:56 +0530

    58780

--- home/models.py
+++ home/models.py
@@ -1,4 +1,7 @@
 from django.db import models
 
+
 class MenuCategory(models.Model):
     name = models.CharField(max_length=100,unique=True)
+    status = models.Foreignkey(OrderStatus, on_delete=models.SET_NULL,null=True)
+
