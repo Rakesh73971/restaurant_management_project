197ea1b362c7aea6fb1aab6b89b3873af55b81d9
Rakesh73971 <nraki522@gmail.com>
2025-11-21 21:39:14 +0530

    58780

--- account/models.py
+++ account/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from .models import OrderStatus
 
 # Create your models here.
+class Order(models.Model):
+    customer = models.ForeignKey('customer',on_delete=models.CASCADE)
+    
+    status = models.ForeignKey(
+        OrderStatus,
+        on_delete = models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    total_amount = models.DecimalField(max_digits=10,decimal_place=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f" Order #{self.id} - {self.customer}"
+
