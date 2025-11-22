c08ad45806355c33d04e0fd0e2f34f38aeaa6049
Rakesh73971 <nraki522@gmail.com>
2025-11-22 21:47:12 +0530

    58782

--- account/models.py
+++ account/models.py
@@ -9,4 +9,14 @@ class Order(models.Model):
         return self.name
 
     
+class Coupon(models.Model):
+    code = models.CharField(max_length=50,unique=True)
+    discount_percentage = models.DecimalField(max_digits=5,decimal_places=2)
+    is_active = models.Boolean(default=True)
+    valid_from = models.DateField()
+    valid_until = models.DateField()
+
+    def __str__(self):
+        return self.code
+
 
