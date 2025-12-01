dd0b3e1d093871ed9864245f2631744bc956eece
Rakesh73971 <nraki522@gmail.com>
2025-12-01 22:14:09 +0530

    61361

--- home/views.py
+++ home/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
-from .models import MenuCategory
-from .serailzers import MenuCategorySerializer
+from .models import MenuCategory,MenuItem
+from .serailzers import MenuCategorySerializer,MenuItemSerializer
 from rest_framework.generics import ListAPIView
+
 
 # Create your views here.
 
 class MenuCategoryListView(ListAPIView):
     queryset = MenuCategory.objects.all()
     serailzer_class = MenuCategorySerializer
+
+class MenuItemListView(ListAPIView):
+    serializer_class = MenuItemSerializer
+
+    def get_queryset(self):
+        return MenuItem.objects.filter(is_featured=True)
 
 
