a1265ce21c793c6854fc389b6127a92363004820
Rakesh73971 <nraki522@gmail.com>
2025-12-01 21:40:58 +0530

    61360

--- orders/models.py
+++ orders/models.py
@@ -10,3 +10,17 @@ class Coupon(models.Model):
 
     def __str__(self):
         return self.code
+
+class Order(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='oders')
+    date = models.DateField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10,decimal_places=2)
+
+    def __str__(self):
+        return f"Order {self.id}"
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(order,related_name='items',on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=255)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10,decimal_places2)
