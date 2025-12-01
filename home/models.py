d19e6715ad319f180ea39830841409e15ac1358b
Rakesh73971 <nraki522@gmail.com>
2025-12-01 22:05:07 +0530

    61361

--- home/models.py
+++ home/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class MenuCategory(models.Model):
     name = models.CharField(max_length=100,unique=True)
     status = models.Foreignkey(OrderStatus, on_delete=models.SET_NULL,null=True)
+    is_featured = models.BooleanField(default=False)
 
