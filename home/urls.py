4850c6b1cd369c6092876782122751275dd90c9b
Rakesh73971 <nraki522@gmail.com>
2025-12-01 22:15:25 +0530

    61361

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import MenuCategoryListView
+from .views import MenuCategoryListView,MenuItemListView
 
 urlpatterns = [
     path('categories/',MenuCategoryListView.as_view(),name="menu-categories")
-    
+    path('featured-menu-items',MenuItemListView.as_view(),name='featured-menu-items')
 ]
