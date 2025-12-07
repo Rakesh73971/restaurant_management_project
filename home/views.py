d6ae827059a98e62add378d12de63a50e25cddbd
Rakesh73971 <nraki522@gmail.com>
2025-12-07 11:41:05 +0530

    61623

--- home/views.py
+++ home/views.py
@@ -3,7 +3,8 @@ from .models import MenuCategory,MenuItem
 from .serailzers import MenuCategorySerializer,MenuItemSerializer
 from rest_framework.generics import ListAPIView
 from django.db.models import Q
-from .pagination import 
+from .pagination import MenuItemPagination
+from rest_framework import viewsets
 
 # Create your views here.
 
@@ -17,7 +18,27 @@ class MenuItemListView(ListAPIView):
     def get_queryset(self):
         return MenuItem.objects.filter(is_featured=True)
 
+class MenuItemSearchViewSet(viewsets.ViewSet):
+    """
+    A Viewset that provides searching for menu items by name.
 
+    """
+    pagination_class = MenuItemPagination
 
+    def list(self,request):
+        query = request.query_params.get('q','')
 
+        if not query:
+            return Response(
+                {'error':'Please provide a search query using ?q='},
+                status = status.HTTP_400_BAD_REQUEST
+            )
+        menu_items = MenuItem.objects.filter(name_icontains=query)
+
+        pagination = self.pagination.class()
+        result_page = pagination.paginate_queryset(menu_items,request)
+
+        serializer = MenuItemSerializer(result_page,many=True)
+
+        return pagination.get_paginated_response(serializer.data)
 
