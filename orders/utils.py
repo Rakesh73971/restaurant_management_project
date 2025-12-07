a3a19cd87c1e477f16784a08b9710c2acbdefa14
Rakesh73971 <nraki522@gmail.com>
2025-12-07 10:55:40 +0530

    61622

--- orders/utils.py
+++ orders/utils.py
@@ -1,5 +1,6 @@
 from django.db.models import Sum
 from .models import Order
+from decimal import Decimal
 
 def get_daily_sales_total(date):
     """
@@ -11,3 +12,19 @@ def get_daily_sales_total(date):
     total = orders.aggregate(total_amount=Sum('total_price'))['total_sum']
 
     return total if total if not None else 0
+
+
+def calculate_tip_amount(order_total,tip_percentage):
+    """
+    Calculate the tip amount based on the order total and tip percentage.
+    Parameters:
+       order_total (Decimal or float): The total bill amount before tip.
+       tip_percentage: The tip percentage (e.g: 15 for 15%).
+    Returns:
+        Decimal:The calculated tip amount rounded to two decimal places.
+    """
+    total = Decimal(order_total)
+    percentage = Decimal(tip_percentage)/Decimal(100)
+    tip_amount = total * percentage
+
+    return round(tip_amount,2)
