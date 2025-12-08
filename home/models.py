7f82a05962f29965023cf398690fb4a4b26ad0e3
Rakesh73971 <nraki522@gmail.com>
2025-12-08 14:59:26 +0530

    61624

--- home/models.py
+++ home/models.py
@@ -41,3 +41,13 @@ class DailySpecial(models.Model):
 
     def __str__(self):
         return self.title
+
+class NutritionalInformation(models.Model):
+    menu_item = models.Foreignkey("MenuItem",on_delete=models.CASCADE,related_name="nutritional_info")
+    calories = models.IntegerField()
+    protein_grams = models.DecimalField(max_digits=5,decimal_places=2)
+    fat_grams = models.DecimalField(max_digits=5,decimal_places=2)
+    carbohydrate_grams = models.DecimalField(max_digits=5,decimal_places=2)
+
+    def __str__(self):
+        return f"{self.menu_item.name} - {self.calories} kcal."
